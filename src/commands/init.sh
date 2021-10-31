69cb604fa357fe6bf06dc247b0b6a0f1d802b178
Jonathan <jonathan@jtw-r.com>
2021-10-30 21:58:48 -0700

    Updated

--- src/commands/init.sh
+++ src/commands/init.sh
@@ -167,6 +167,8 @@ mkdir -p .github/workflows
 echo "Operating directory: ${PWD##*/}"
 echo "$SCRIPT_DIR/../"
 cp "$SCRIPT_DIR/../"setup_assets/deploy-to-branch.yml .github/workflows/deploy-to-branch.yml
+cd "$SCRIPT_DIR/../"
+ls
 #cp ./src/setup_assets/deploy-to-branch.yml .github/workflows/deploy-to-branch.yml
 
 npm set-script deployment-branch "echo $deploy_branch_name"
